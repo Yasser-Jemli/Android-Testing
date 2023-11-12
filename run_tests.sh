d72f694fc2fb00b9bc3989f032e794457b21153b
Yasser-Jemli <yasserjamli37@gmail.com>
2023-11-12 17:36:58 +0100

    Working on docker env
    Signed-off-by: Yasser-Jemli <yasserjamli37@gmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -5,8 +5,8 @@
 sleep 10
 
 # Define the paths to your test suites
-TEST_SUITE_1="test_suites/android_tests.robot"
-TEST_SUITE_2="test_suites/other_tests.robot"
+TEST_SUITE_1="test_suites/first_test_case.robot"
+
 
 # Set your variables as command-line arguments
 DEVICE_NAME="YourDeviceName"
@@ -17,6 +17,4 @@ APP_ACTIVITY=".MainActivity"
 
 # Run the test suites
 robot -d results -v DEVICE_NAME:"$DEVICE_NAME" -v PLATFORM_NAME:"$PLATFORM_NAME" -v PLATFORM_VERSION:"$PLATFORM_VERSION" -v APP_PACKAGE:"$APP_PACKAGE" -v APP_ACTIVITY:"$APP_ACTIVITY" "$TEST_SUITE_1"
-robot -d results -v DEVICE_NAME:"$DEVICE_NAME" -v PLATFORM_NAME:"$PLATFORM_NAME" -v PLATFORM_VERSION:"$PLATFORM_VERSION" -v APP_PACKAGE:"$APP_PACKAGE" -v APP_ACTIVITY:"$APP_ACTIVITY" "$TEST_SUITE_2"
-
 
